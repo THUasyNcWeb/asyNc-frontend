7eb41e4ea847cae5cdde93ec6eb7fc7fcd9f13fc
郑友捷 <2021010771@secoder.net>
2022-10-10 21:23:31 +0800

    :sparkles: feat:add the top navigation bar in the user page
    Now the user page has the top navigation bar, and more function will be achieved in it.

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -9,7 +9,29 @@
 <template>
   <n-space vertical size="large">
     <n-layout style="width:100%;height: 100%;">
-      <n-layout-header style="height:64px">用户主页</n-layout-header>
+      <n-layout-header bordered style="height:50px;text-align:center;" >
+        <tr style="height:50px; ">
+          <th>
+            <n-image :src="require(`@/assets/log-news.png`)"
+              style="height:30px;"></n-image>
+          </th>
+          <n-divider :vertical=true />
+          <th style="width:90%">
+            <n-h2 style="text-align:left">
+              用户主页
+            </n-h2>
+          </th>
+          <th style="width:5%">
+            <router-link to='/'>搜索主页</router-link>
+          </th>
+          <n-divider :vertical=true />
+          <th style="width:5%">
+            <n-dropdown trigger = "hover" :options="options" @select="handleSelect">
+                {{username}}
+            </n-dropdown>
+          </th>
+        </tr>
+      </n-layout-header>
       <n-layout has-sider>
         <n-layout-sider bordered content-style="padding: 24px;">
           <n-menu :options="menuOptions" />
@@ -26,7 +48,7 @@
 <script>
 import {h,defineComponent} from "vue"
 import { RouterLink } from 'vue-router'
-import {NAnchorLink, NAnchor, NLayout,NLayoutSider, NLayoutContent,NH2,NSpace,NButton,NLayoutHeader,NMenu,NInput} from 'naive-ui'
+import {NAnchorLink, NAnchor,NImage,NDivider,NDropdown, NLayout,NLayoutSider, NLayoutContent,NH2,NSpace,NButton,NLayoutHeader,NMenu,NInput} from 'naive-ui'
 export default defineComponent({
   components: {
     NAnchorLink,
@@ -40,6 +62,9 @@ export default defineComponent({
     NLayoutHeader,
     NMenu,
     NInput,
+    NImage,
+    NDivider,
+    NDropdown,
   },
   setup() {
     const menuOptions = [
@@ -71,10 +96,22 @@ export default defineComponent({
       },
     ]
     // 加入侧边导航栏菜单，每一个选项对应一个路由跳转
+
+    const options = [
+        {
+            label:"切换账号",
+            key:"Home",
+        },
+        {
+            label:"退出登录",
+            key:"edit",
+        }
+    ]
     const username = sessionStorage.getItem("username")
     return {
       menuOptions,
-      username
+      username,
+      options,
     }
   }
 })
