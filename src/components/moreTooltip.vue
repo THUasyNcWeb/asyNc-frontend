cd8df792cbeeaafb8df1cdec73a2f67db9f5f6a7
郑友捷 <2021010771@secoder.net>
2022-11-02 08:42:33 +0800

    :sparkles: feat: add the more function in the news page  asyNc-web#106
    Now it can select more kinds of news in the news page.
    
    partly finish asyNc-web#106

---
+++ src/components/moreTooltip.vue
@@ -0,0 +1,16 @@
+<template>
+  <n-tooltip trigger="hover">
+    <template #trigger>
+      <n-button> {{props.more_content}}</n-button>
+    </template>
+    点我查看更多类别的新闻
+  </n-tooltip>
+</template>
+
+<script setup lang="ts">
+import { NTooltip,NButton } from 'naive-ui';
+import {defineProps} from 'vue' 
+const props = defineProps<{
+    more_content: String,
+}>();
+</script>
--- src/views/content/NewsHome.vue
+++ src/views/content/NewsHome.vue
@@ -7,22 +7,36 @@
  -->
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive,h } from 'vue';
 import '@/mock/SearchPage.mock'
 import API from "../../store/axiosInstance"
 import NewsCategory from '@/components/NewsCategory.vue'
+import moreTooltip from '@/components/moreTooltip.vue';
 import { 
     NTabs,
     NTabPane,
+    NButton,
+    NSpace,
 } from 'naive-ui'
 // 按需引入naive-ui组件
 // 之后可能会把上述引入集中在一个固定的ts文件中
 
 
-const state = reactive({all_news: new Array(),window_width:document.body.clientWidth * 0.4, all_category:new Array(), now_category:"home"})
+const state = reactive({all_news: new Array(),
+    window_width:document.body.clientWidth * 0.4, 
+    all_category:new Array(), 
+    now_category:"home", 
+    more_content:'更多'})
 // let router = useRouter()
 
-state.all_category.push('home','genshin')
+state.all_category.push(
+    {key:'home', label:'首页'},
+    {key:'genshin', label:'原神'},
+    {key:'tsinghua', label:'清华'},
+    {key:'hometown', label:'家乡'},
+    {key:'tech', label:'科技'},
+    {key:'life', label:'生活'},
+)
 
 function get_news(category:string) {
     console.log(category)
@@ -44,21 +58,38 @@ function get_news(category:string) {
 
 }
 
-// get_news('home')
-
+function more_news(){
+    return h(moreTooltip, {
+        more_content:state.more_content,
+    })
+}
 </script>
   
 <template>
     <body>
         <n-tabs :value="state.now_category" 
-        @update:value="state.now_category = $event;get_news(state.now_category)" 
+        @update:value="state.now_category=$event;get_news(state.now_category)" 
         type="line" animated :tabs-padding=state.window_width 
-        pane-style="margin-left:20%;" 
+        pane-style="margin-left:20%;"
         size="large" default-value="home">
-            <n-tab-pane v-for="item in state.all_category" :key="item" :name=item :tab=item>
+            <n-tab-pane v-for="item in state.all_category" :key="item.key" :name=item.key :tab=item.label>
                 <NewsCategory :news="state.all_news"/>
             </n-tab-pane>
-        </n-tabs>
+            <n-tab-pane name="更多" :tab=more_news>
+                <n-space v-if="state.more_content=='更多'" style="max-width:70%">
+                    <n-button>
+                        时尚
+                    </n-button>
+                    <n-button>
+                        政治
+                    </n-button>
+                    <n-button>
+                        体育
+                    </n-button>
+                </n-space>
+                <NewsCategory v-else :news="state.all_news"/>
+            </n-tab-pane>
+         </n-tabs>
     </body>
 </template>
   
