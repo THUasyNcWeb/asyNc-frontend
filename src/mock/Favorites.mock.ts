1442d1923f354f7096dfbc9723fd40ea4b337ef7
abmfy <2021010787@secoder.net>
2022-11-14 21:04:50 +0800

    :clown_face: test: add mock for favorites asyNc-web#173

--- src/mock/Favorites.mock.ts
+++ src/mock/Favorites.mock.ts
@@ -21,6 +21,7 @@ let favList = [
     media: '腾讯新闻',
     url: 'https://www.bilibili.com/video/BV1GJ411x7h7',
     pub_time: new Date().toJSON(),
+    visit_time: new Date().toJSON(),
   },
   {
     id: 2,
@@ -35,14 +36,15 @@ let favList = [
     media: '亦可赛艇新闻',
     url: 'https://www.bilibili.com/video/BV1GJ411x7h7',
     pub_time: new Date(1926, 7, 17).toJSON(),
-    picture_url: 'https://app.moegirl.org.cn/萌百娘表情包/萌百娘表情包2/钻地.png',
+    image_url: 'https://app.moegirl.org.cn/萌百娘表情包/萌百娘表情包2/钻地.png',
+    visit_time: new Date().toJSON(),
   },
   {
     id: 4,
     title: '有图片的很长的非常好新闻',
     media: '有仙则名新闻',
     url: 'https://www.bilibili.com/video/BV1GJ411x7h7',
-    picture_url: 'https://www.desmos.com/assets/img/homepage-student.png',
+    image_url: 'https://www.desmos.com/assets/img/homepage-student.png',
     pub_time: new Date(9876, 4, 43).toJSON(),
   },
   {
@@ -50,15 +52,16 @@ let favList = [
     title: 'Breaking News',
     media: 'Foobar News',
     url: 'https://breaking.news',
-    picture_url: 'https://breaking.news/picture.png',
+    image_url: 'https://breaking.news/picture.png',
     pub_time: '2022-10-21T19:02:16.305Z',
+    visit_time: new Date().toJSON(),
   },
   {
     id: 6,
     title: '萨满祭司 (卡牌游戏《炉石传说》中的职业)',
     media: '百度百科',
     url: 'https://baike.baidu.com/item/萨满祭司/49863467?fr=aladdin',
-    picture_url: 'https://bkimg.cdn.bcebos.com/pic/3801213fb80e7beccf8ad6f12b2eb9389b506b4a?x-bce-process=image/resize,m_lfit,w_128,limit_1',
+    image_url: 'https://bkimg.cdn.bcebos.com/pic/3801213fb80e7beccf8ad6f12b2eb9389b506b4a?x-bce-process=image/resize,m_lfit,w_128,limit_1',
     pub_time: '2022-10-24T19:02:16.305Z',
   }
 ];
