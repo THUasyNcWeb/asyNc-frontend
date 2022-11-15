6dc6276edc25247c8f24514ff1517f798c9f94a2
abmfy <2021010787@secoder.net>
2022-11-15 11:58:01 +0800

    :sparkles: feat: add view all button asyNc-web#173

--- src/components/FavoriteNewsPanel.vue
+++ src/components/FavoriteNewsPanel.vue
@@ -3,33 +3,32 @@
  * @Author: 王博文
  * @Date: 2022-11-07 00:40
  * @LastEditors: 王博文
- * @LastEditTime: 2022-11-15 11:29
+ * @LastEditTime: 2022-11-15 11:57
 -->
 
 <template>
   <n-layout style="height: 60vh">
     <n-empty v-if="!news.length" size="large" />
-      <n-list v-else hoverable>
-        <n-list-item v-for="item in news">
-          <n-a :href="item.url" @click="newsClick(item.id)" style="text-decoration: none;">
-            <news-panel-entry :id="item.id" :url="item.url" :title="item.title"
-            :media="item.media" :image_url="item.image_url" :visit_time="item.visit_time"/>
-          </n-a>
-        </n-list-item>
-        <n-list-item v-if="news.length >= maxNewsCount">
-          <router-link :to="morePath">
-            <n-a>
-              更多...
-            </n-a>
-          </router-link>
-        </n-list-item>
-      </n-list>
+    <n-list v-else hoverable>
+      <n-list-item v-for="item in news">
+        <n-a :href="item.url" @click="newsClick(item.id)" style="text-decoration: none;">
+          <news-panel-entry :id="item.id" :url="item.url" :title="item.title"
+          :media="item.media" :image_url="item.image_url" :visit_time="item.visit_time"/>
+        </n-a>
+      </n-list-item>
+    </n-list>
   </n-layout>
+  <router-link :to="morePath" style="text-decoration: none">
+    <n-button>
+      查看全部
+    </n-button>
+  </router-link>
 </template>
 
 <script setup lang="ts">
 import {
   NA,
+  NButton,
   NEmpty,
   NLayout,
   NList,
@@ -41,9 +40,6 @@ import NewsPanelEntry from '@/components/NewsPanelEntry.vue';
 import { newsClick } from '@/main';
 
 import type { News } from '@/components/NewsPanelEntry.vue';
-
-// Max count of news shown in a panel
-const maxNewsCount = 10;
 
 defineProps<{
   news: News[],
