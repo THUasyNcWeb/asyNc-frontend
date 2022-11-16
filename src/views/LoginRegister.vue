e583b7984d1caea01026ef3e9e07b5eb9739d20a
kming <kming_l@163.com>
2022-11-16 20:39:04 +0800

    :lipstick: feat: optimize the UI of the login and registration page asyNc-web#208

--- src/views/LoginRegister.vue
+++ src/views/LoginRegister.vue
@@ -7,11 +7,11 @@
 -->
 <template>
   <div class="first_div">
-    <n-card :bordered="false" size="large" class="card">
+    <n-card size="large" class="card" hoverable:="true" style="">
       <div style="width: 300px;">
-        <n-space justify="space-between">
+        <n-space justify="space-between" size="large">
           <img src="../assets/asyNc_avatar.png" style="width:80px;height:80px;overflow: hidden;" />
-          <n-gradient-text type="primary" :size="26.5" style="vertical-align: 60%; margin-left: 42px;">asyNc检索系统
+          <n-gradient-text type="primary" :size="26.5" style="margin-top: 20px;">asyNc检索系统
           </n-gradient-text>
         </n-space>
         <n-grid y-gap="12" :cols="1">
@@ -20,23 +20,33 @@
               from: 'rgb(24, 160, 88)',
               to: 'rgb(24, 160, 88)'
             }" size="20" style="vertical-align: -60%;">
-              账密{{ title }}
+              账密{{ state.title }}
             </n-gradient-text>
           </n-grid-item>
           <n-grid-item>
-            <n-input size="large" placeholder="请输入用户名" v-model:value="username" style="" />
+            <n-space vertical>
+              <n-input size="large" placeholder="请输入用户名" v-model:value="username" style="" />
+              <n-text v-if="state.title == '注册'" depth="3" style="font-size: small; margin-top: 0%; padding-top: 0%;">
+                |&nbsp 用户名为包含 "中英文数字 _ -" 在内的 1-14 位字符，且首字符需为中文或英文
+              </n-text>
+            </n-space>
           </n-grid-item>
           <n-grid-item>
-            <n-input type="password" size="large" placeholder="请输入密码" v-model:value="password"
-              show-password-on="click" />
+            <n-space vertical>
+              <n-input type="password" size="large" placeholder="请输入密码" v-model:value="password"
+                show-password-on="click" />
+              <n-text v-if="state.title == '注册'" depth="3" style="font-size: small; margin-top: 0%; padding-top: 0%;">
+                |&nbsp 密码为包含 "英文 _ -" 在内的 8-14 个字符
+              </n-text>
+            </n-space>
           </n-grid-item>
-          <n-grid-item v-if="title == '注册'">
+          <n-grid-item v-if="state.title == '注册'">
             <n-input type="password" size="large" placeholder="请再次输入密码" v-model:value="re_password"
               show-password-on="click" />
           </n-grid-item>
           <n-grid-item style="text-align:center">
             <n-button round style="width:100%; margin-top:5%;" type="success" @click="() => {
-              if (title == '登录') {
+              if (state.title == '登录') {
                 login()
               }
               else {
@@ -46,10 +56,19 @@
               确定
             </n-button>
           </n-grid-item>
-          <n-grid-item v-if="title == '登录'" style="text-align:center;">
-            <n-button style="width:100%;" type="tertiary" @click="() => { handleRegister() }">
-              注册
-            </n-button>
+          <n-grid-item style="text-align:center;">
+            <n-space justify="space-between" size="large">
+              <n-button style="width:140px;" type="tertiary" @click="() => { handleHomePage() }">
+                主页
+              </n-button>
+              <n-button v-if="state.title == '登录'" style="width:140px;" type="tertiary"
+                @click="() => { handleRegister() }">
+                注册
+              </n-button>
+              <n-button v-else style="width:140px;" type="tertiary" @click="() => { handleLogin() }">
+                返回登录
+              </n-button>
+            </n-space>
           </n-grid-item>
           <!-- 登录界面与注册界面进行勾连 -->
         </n-grid>
@@ -58,10 +77,9 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router';
-import { ref, Ref } from 'vue'
+<script setup lang="ts">
+import router from '@/router';
+import { ref, Ref, reactive, watch } from 'vue'
 import API from "../store/axiosInstance"
 import {
   NCard,
@@ -70,135 +88,123 @@ import {
   NGrid,
   NGridItem,
   NButton,
+  NSpace,
+  NText,
 } from 'naive-ui'
 
-export default defineComponent({
-  components: {
-    NCard,
-    NGradientText,
-    NInput,
-    NGrid,
-    NGridItem,
-    NButton,
-  },
-  setup() {
-    var title: string = ""
-    let router = useRouter()
-    console.log(router.currentRoute.value.path);
-    if (router.currentRoute.value.path == "/login") {
-      title = "登录"
-    }
-    else if (router.currentRoute.value.path == "/register") {
-      title = "注册"
-    }
-    else {
-      alert("异常访问")
-      router.push("/")
-    }
-    var username: Ref<string> = ref("")
-    var password: Ref<string> = ref("")
-    var re_password: Ref<string> = ref("")
+var title: string = ""
+const state = reactive({ title })
 
-    function handleRegister() {
-      router.push("/register")
-    }
-
-    function judgeUser(username: string) {
-      if (username.length > 14) {
-        return false;
-      }
-      return /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(username)
-    }
-
-    function judgePassword(password: string) {
-      if (password.length > 14 || password.length < 8) {
-        return false;
-      }
-      return /^[-A-Za-z0-9_]*$/.test(password)
-    }
-
-    function judgeRepassword() {
-      if (re_password.value != password.value) {
-        return false;
-      }
-      return judgePassword(re_password.value)
-    }
-    function login() {
-      /**
-      * @description: 登录功能
-      * @return {void}
-      */
-      API({
-        url: 'login',
-        method: 'post',
-        data: {
-          "user_name": username.value,
-          "password": password.value,
-        }
-      }).then((res) => {
-        // 对登录接口请求成功
-        console.log(res)
-        window.localStorage.setItem("token", res.data.data.token)
-        // 存储token
-        alert("登录成功，欢迎来到asyNc！")
-        router.push("/")
-      }).catch((error) => {
-        console.log(error);
-        alert("用户名或密码错误")
-      })
-    }
-    function register() {
-      /**
-      * @description: 注册功能
-      * @return {void}
-      */
-      if (judgeUser(username.value) == false) {
-        alert("输入用户名不规范")
-        return;
-      }
-      if (judgePassword(password.value) == false) {
-        alert("输入密码不规范")
-        return;
-      }
-      if (judgeRepassword() == false) {
-        alert("重新输入密码不规范")
-        return;
-      }
-      console.log(username.value)
-      console.log(password.value)
-      API({
-        url: 'register',
-        method: 'post',
-        data: {
-          "user_name": username.value,
-          "password": password.value,
-        }
-      }).then((res) => {
-        // 对注册接口请求成功
-        console.log(res)
-        window.localStorage.setItem("token", res.data.data.token)
-        // 存储token
-        alert("注册成功，欢迎来到asyNc！")
-        router.push("/")
-      }).catch((error) => {
-        console.log(error);
-        alert("注册失败")
-      })
-    }
-    return {
-      username,
-      password,
-      title,
-      login,
-      register,
-      re_password,
-      judgeUser,
-      judgePassword,
-      judgeRepassword,
-      handleRegister,
-    }
+watch(() => router.currentRoute.value.path, (newValue, _oldValue) => {
+  console.log('watch', newValue);
+  if (newValue == "/login") {
+    state.title = "登录"
+  } else {
+    state.title = "注册"
   }
-})
+}, { immediate: true })
+var username: Ref<string> = ref("")
+var password: Ref<string> = ref("")
+var re_password: Ref<string> = ref("")
+
+function handleRegister() {
+  router.push('/register')
+  state.title = "注册"
+}
+
+function handleLogin() {
+  router.push('/login')
+  state.title = "登录"
+}
+
+function handleHomePage() {
+  router.push("/")
+}
+
+function judgeUser(username: string) {
+  if (username.length > 14) {
+    return false;
+  }
+  return /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(username)
+}
+
+function judgePassword(password: string) {
+  if (password.length > 14 || password.length < 8) {
+    return false;
+  }
+  return /^[-A-Za-z0-9_]*$/.test(password)
+}
+
+function judgeRepassword() {
+  if (re_password.value != password.value) {
+    return false;
+  }
+  return judgePassword(re_password.value)
+}
+
+function login() {
+  /**
+  * @description: 登录功能
+  * @return {void}
+  */
+  API({
+    url: 'login',
+    method: 'post',
+    data: {
+      "user_name": username.value,
+      "password": password.value,
+    }
+  }).then((res) => {
+    // 对登录接口请求成功
+    console.log(res)
+    window.localStorage.setItem("token", res.data.data.token)
+    // 存储token
+    alert("登录成功，欢迎来到asyNc！")
+    router.push("/")
+  }).catch((error) => {
+    console.log(error);
+    alert("用户名或密码错误")
+  })
+}
+
+function register() {
+  /**
+  * @description: 注册功能
+  * @return {void}
+  */
+  if (judgeUser(username.value) == false) {
+    alert("输入用户名不规范")
+    return;
+  }
+  if (judgePassword(password.value) == false) {
+    alert("输入密码不规范")
+    return;
+  }
+  if (judgeRepassword() == false) {
+    alert("重新输入密码不规范")
+    return;
+  }
+  console.log(username.value)
+  console.log(password.value)
+  API({
+    url: 'register',
+    method: 'post',
+    data: {
+      "user_name": username.value,
+      "password": password.value,
+    }
+  }).then((res) => {
+    // 对注册接口请求成功
+    console.log(res)
+    window.localStorage.setItem("token", res.data.data.token)
+    // 存储token
+    alert("注册成功，欢迎来到asyNc！")
+    router.push("/")
+  }).catch((error) => {
+    console.log(error);
+    alert("注册失败")
+  })
+}
 </script>
 
 <style>
@@ -219,13 +225,16 @@ export default defineComponent({
 }
 
 .card {
-  width: 330px;
-  height: 420px;
+  width: 364px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  border-radius: 25px;
+  border: 5px solid;
 }
 
-input:-webkit-autofill { box-shadow: 0 0 0px 1000px white inset !important;}
+input:-webkit-autofill {
+  box-shadow: 0 0 0px 1000px white inset !important;
+}
 </style>
