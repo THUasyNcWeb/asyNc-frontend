2dab15413b0f6b1cd3a3bc3d68e6d09169585837
郑友捷 <2021010771@secoder.net>
2022-11-17 14:51:23 +0800

    :lipstick: style: change the whole layout of the user's page asyNc-web#192
    Now it determines the whole layout of the user's information's page.
    
    partly finish asyNc-web#192

--- src/components/UserInformation.vue
+++ src/components/UserInformation.vue
@@ -1,44 +1,31 @@
 <template>
-<n-grid :item-responsive="true" :x-gap="16" :y-gap="16">
+  <n-grid :item-responsive="true" :x-gap="16" :y-gap="16">
     <n-grid-item span="0:24 640:24 1024:10">
       <n-space :vertical="true" :size="16">
-        <n-card title="用户基本信息" :bordered="true" size="large" class="shadow-sm rounded-16px">
+        <n-card :bordered="true" size="large" style="height:300px" class="card_border">
 					<n-space :vertical="true" :size="16" style="text-align:center" >
-						<UserAvatar style="margin-left:30%" :width="120" :height="120" :image_code="props.user.avatar" @changeavatar="update_avatar"></UserAvatar>
+						<UserAvatar style="margin-left:35%" :width="120" :height="120" :image_code="props.user.avatar" @changeavatar="update_avatar"></UserAvatar>
 						<n-text style="font-size:30px">
               欢迎，{{props.user.user_name}}！
 						</n-text>
-            <n-text style="color:#808080">
-              个性签名：{{props.user.signature}}
-            </n-text>
 					</n-space>
-        </n-card>
-        <n-card title="最近浏览" :bordered="true" size="large" class="shadow-sm rounded-16px">
-          <WordChart :tags="props.user.tags"></WordChart>
         </n-card>
       </n-space>
     </n-grid-item>
     <n-grid-item span="0:24 640:24 1024:14">
-      <n-space :vertical="true" :size="16">
-        <EditInformation :user="props.user"/>
-        <n-card title="小彩蛋" :bordered="true" size="large" class="shadow-sm rounded-16px">
-          <n-space  :vertical="true" :size="16" style="text-align:center">
-            <n-image :src="img" :width=150  />
-            <n-text>
-              请支援我们！清华的可怜孩子连饭都吃不饱了！  
-            </n-text>
-          </n-space>
-
-        </n-card>
-      </n-space>
-      
+      <n-card title="最近浏览" :bordered="true" size="large" class="card_border">
+        <WordChart :tags="props.user.tags"></WordChart>
+      </n-card>
+    </n-grid-item>
+    <n-grid-item span="0:24 640:24 1024:24">
+      <EditInformation :user="props.user"/>
     </n-grid-item>
   </n-grid>
 </template>
 
 <script setup lang="ts">
 
-import { NGrid,NGridItem, NCard, NSpace,NText,NImage } from 'naive-ui';
+import { NGrid,NGridItem, NCard, NSpace,NText } from 'naive-ui';
 import { defineProps,defineEmits } from 'vue'
 import UserAvatar from '@/components/UserAvatar.vue'
 import EditInformation from './EditInformation.vue';
@@ -55,10 +42,19 @@ const props = defineProps<{
   user:UserInfo,
 }>();
 const emits = defineEmits(['change-info','change-avatar']);
-const img = require("@/assets/QRcode.jpg")
+// const img = require("@/assets/asyNc.png")
 
 function update_avatar(img_code:string) {
   emits('change-avatar', img_code)
   // 更新主页面
 }
 </script>
+
+
+<style scoped>
+.card_border {
+  border-radius: 20px;
+  border-color: #000000;
+  /* background-color: #F5F5F5; */
+}
+</style>
