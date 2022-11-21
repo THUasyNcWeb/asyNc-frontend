a3c9b1f9c0006880c1686c94f7c0430a92302662
郑友捷 <2021010771@secoder.net>
2022-11-21 16:20:25 +0800

    :recycle: refactor: format the code style in the typescript asyNc-web#223
    This commit format the code style in the typescript file.
    
    partly finish asyNc-web#223

--- src/components/EditInformation.vue
+++ src/components/EditInformation.vue
@@ -5,7 +5,7 @@
  * @LastEditors: 郑友捷
  * @LastEditTime: 2022-11-18 17:31
  -->
- <template>
+<template>
   <div style="width: 90%; margin: auto">
     <n-grid :item-responsive="true" :x-gap="16" :y-gap="16">
       <n-grid-item span="0:24 640:24 1024:24">
@@ -113,18 +113,33 @@
 </template>
 
 <script setup lang="ts">
-import WordChart from './WordChart.vue';
-import { PersonOutline,MailOutline,PencilOutline,EyeOutline } from '@vicons/ionicons5';
-import { defineProps, reactive,ref,defineEmits,watch } from 'vue'
-import {NText,NGrid,NGridItem,NButton,NInput,NSpace,NIcon,NH2,useMessage} from 'naive-ui'
-import API from '@/store/axiosInstance'
+import WordChart from "./WordChart.vue";
+import {
+  PersonOutline,
+  MailOutline,
+  PencilOutline,
+  EyeOutline,
+} from "@vicons/ionicons5";
+import { defineProps, reactive, ref, defineEmits, watch } from "vue";
+import {
+  NText,
+  NGrid,
+  NGridItem,
+  NButton,
+  NInput,
+  NSpace,
+  NIcon,
+  NH2,
+  useMessage,
+} from "naive-ui";
+import API from "@/store/axiosInstance";
 export interface UserInfo {
-  id: string,
-  user_name: string,
-  signature: string,
-  tags: object,
-  mail: string,
-  avatar: string,
+  id: string;
+  user_name: string;
+  signature: string;
+  tags: object;
+  mail: string;
+  avatar: string;
 }
 
 // Message box
@@ -133,144 +148,169 @@ const message = useMessage();
 // const userRef:UserInfo = inject('userRef')
 
 const props = defineProps<{
-  user:UserInfo,
+  user: UserInfo;
 }>();
-const emits = defineEmits(['change-info','change-avatar']);
+const emits = defineEmits(["change-info", "change-avatar"]);
 
 const state = reactive({
-	edit_status:false,
-	button_text:'编辑信息', 
-	title: "欢迎",
-	user: {
-		mail:ref(null), 
-		signature:ref(null), 
-		user_name:ref(null),
-		mail_valid:true,
-		user_valid:true,
-		sign_valid:true,
-	},
-	send_valid:true,
-})
+  edit_status: false,
+  button_text: "编辑信息",
+  title: "欢迎",
+  user: {
+    mail: ref(null),
+    signature: ref(null),
+    user_name: ref(null),
+    mail_valid: true,
+    user_valid: true,
+    sign_valid: true,
+  },
+  send_valid: true,
+});
 
-initModify()
+initModify();
 watch(
   () => props.user,
   () => {
-    initModify()
+    initModify();
   }
-)
-function initModify(){
-	state.user.mail = ref(props.user.mail)
-	state.user.signature = ref(props.user.signature)
-	state.user.user_name = ref(props.user.user_name)
-	if ((state.user.signature == "" || state.user.signature == null || state.user.signature == undefined)) {
-		state.user.signature = "这个人很懒，什么都没留下"
-	}
-  state.title = "欢迎，" + state.user.user_name + "！"
-  state.user.mail_valid = true
-  state.user.user_valid = true
-  state.user.sign_valid = true
+);
+function initModify() {
+  state.user.mail = ref(props.user.mail);
+  state.user.signature = ref(props.user.signature);
+  state.user.user_name = ref(props.user.user_name);
+  if (
+    state.user.signature == "" ||
+    state.user.signature == null ||
+    state.user.signature == undefined
+  ) {
+    state.user.signature = "这个人很懒，什么都没留下";
+  }
+  state.title = "欢迎，" + state.user.user_name + "！";
+  state.user.mail_valid = true;
+  state.user.user_valid = true;
+  state.user.sign_valid = true;
 }
 
-function returnInfo(){
-	state.edit_status = false
-	state.button_text = '编辑信息'
-  state.title = "欢迎，" + state.user.user_name.value + "！"
-	state.send_valid = true
-  initModify()
+function returnInfo() {
+  state.edit_status = false;
+  state.button_text = "编辑信息";
+  state.title = "欢迎，" + state.user.user_name.value + "！";
+  state.send_valid = true;
+  initModify();
 }
 
-function changeStatus(){
-	if(state.edit_status == false) {
-		state.send_valid = true
-		state.edit_status = true
-		state.button_text = '保存'
-		initModify()
-    state.title = '编辑信息'
-		checkMail()
-		checkSignature()
-		checkUsername()
-		checkSend()
-	}
-	else {
-		API({
-			headers:{"Authorization": window.localStorage.getItem("token")},
-			url:'modifyuserinfo', 
-			method:'post',
-			data:{
-				new_user_name: state.user.user_name,
-				signature: state.user.signature,
-				mail: state.user.mail,
-			}
-			// 根据不同类别，把类别放在了对应的请求参数中
-		}).then((res)=>{
-			console.log(res)
-			state.edit_status = false
-			state.button_text = '编辑信息'
-			state.title = '详细信息'
-			emits("change-info", state.user.user_name, state.user.signature, state.user.mail)
-      message.success('修改成功🥳')
-      // alert("修改成功")
-		}).catch((error) => {
-			console.log(error);
-			// alert("修改失败")
-      message.error('修改失败😢')
-		});
-		// 发送接口
-	}
+function changeStatus() {
+  if (state.edit_status == false) {
+    state.send_valid = true;
+    state.edit_status = true;
+    state.button_text = "保存";
+    initModify();
+    state.title = "编辑信息";
+    checkMail();
+    checkSignature();
+    checkUsername();
+    checkSend();
+  } else {
+    API({
+      headers: { Authorization: window.localStorage.getItem("token") },
+      url: "modifyuserinfo",
+      method: "post",
+      data: {
+        new_user_name: state.user.user_name,
+        signature: state.user.signature,
+        mail: state.user.mail,
+      },
+      // 根据不同类别，把类别放在了对应的请求参数中
+    })
+      .then((res) => {
+        console.log(res);
+        state.edit_status = false;
+        state.button_text = "编辑信息";
+        state.title = "详细信息";
+        emits(
+          "change-info",
+          state.user.user_name,
+          state.user.signature,
+          state.user.mail
+        );
+        message.success("修改成功🥳");
+        // alert("修改成功")
+      })
+      .catch((error) => {
+        console.log(error);
+        // alert("修改失败")
+        message.error("修改失败😢");
+      });
+    // 发送接口
+  }
 }
 
 function checkUsername() {
-	if (!(state.user.user_name == "" || state.user.user_name == null || state.user.user_name == undefined)) {
-		state.user.user_name = state.user.user_name.trim()
-		const now_name = state.user.user_name
-		state.user.user_valid = /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(now_name)
-		checkSend()
-		return;
-	}
-	state.user.user_valid = false
-	state.send_valid = false
+  if (
+    !(
+      state.user.user_name == "" ||
+      state.user.user_name == null ||
+      state.user.user_name == undefined
+    )
+  ) {
+    state.user.user_name = state.user.user_name.trim();
+    const now_name = state.user.user_name;
+    state.user.user_valid =
+      /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(now_name);
+    checkSend();
+    return;
+  }
+  state.user.user_valid = false;
+  state.send_valid = false;
 }
 
-function checkMail(){
-	var reg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
-	console.log(now_mail)
-	if (!(state.user.mail == "" || state.user.mail == null || state.user.mail == undefined)) {
-		state.user.mail = state.user.mail.trim()
-		var now_mail = state.user.mail
-		state.user.mail_valid =  reg.test(now_mail); //符合条件与否
-		checkSend()
-		return;
-	}
-	state.user.mail_valid = true; //允许为空
-	checkSend()
+function checkMail() {
+  var reg =
+    /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
+  console.log(now_mail);
+  if (
+    !(
+      state.user.mail == "" ||
+      state.user.mail == null ||
+      state.user.mail == undefined
+    )
+  ) {
+    state.user.mail = state.user.mail.trim();
+    var now_mail = state.user.mail;
+    state.user.mail_valid = reg.test(now_mail); //符合条件与否
+    checkSend();
+    return;
+  }
+  state.user.mail_valid = true; //允许为空
+  checkSend();
 }
 
 function checkSignature() {
-	if (!(state.user.signature == "" || state.user.signature == null || state.user.signature == undefined)) {
-		state.user.signature = state.user.signature.trim()
-		var now_signature = state.user.signature
-		console.log(now_signature)
-		state.user.sign_valid = now_signature.length <= 50 
-		checkSend()
-		return;
-	}
-	state.user.sign_valid = true
-	checkSend()
+  if (
+    !(
+      state.user.signature == "" ||
+      state.user.signature == null ||
+      state.user.signature == undefined
+    )
+  ) {
+    state.user.signature = state.user.signature.trim();
+    var now_signature = state.user.signature;
+    console.log(now_signature);
+    state.user.sign_valid = now_signature.length <= 50;
+    checkSend();
+    return;
+  }
+  state.user.sign_valid = true;
+  checkSend();
 }
 
-function checkSend(){
-	if(state.user.sign_valid && state.user.mail_valid && state.user.user_valid) {
-		state.send_valid = true
-	}
-	else {
-		state.send_valid = false
-	}
+function checkSend() {
+  if (state.user.sign_valid && state.user.mail_valid && state.user.user_valid) {
+    state.send_valid = true;
+  } else {
+    state.send_valid = false;
+  }
 }
-
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
