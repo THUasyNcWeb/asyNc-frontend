dfd4916968e53d6446bec789925d1c6f2c65daae
郑友捷 <2021010771@secoder.net>
2022-11-21 16:15:06 +0800

    :recycle: refactor: format the code in editinformation.vue asyNc-web#223
    This commit formats the code in the editinformation.vue.
    
    partly finish asyNc-web#223

--- src/components/EditInformation.vue
+++ src/components/EditInformation.vue
@@ -5,88 +5,108 @@
  * @LastEditors: 郑友捷
  * @LastEditTime: 2022-11-18 17:31
  -->
-<template>
-  <div style="width:90%;margin: auto; ">
+ <template>
+  <div style="width: 90%; margin: auto">
     <n-grid :item-responsive="true" :x-gap="16" :y-gap="16">
       <n-grid-item span="0:24 640:24 1024:24">
         <n-space justify="space-between">
           <n-h2>
-            {{state.title}}
+            {{ state.title }}
           </n-h2>
           <n-space>
-            <n-button v-if="state.edit_status==true" style="border-radius: 20px;font-size:15px" type="warning" @click="returnInfo" >
+            <n-button
+              v-if="state.edit_status == true"
+              style="border-radius: 20px; font-size: 15px"
+              type="warning"
+              @click="returnInfo"
+            >
               返回
             </n-button>
-            <n-button style="border-radius: 20px;font-size:15px" type="success" @click="changeStatus" :disabled="!state.send_valid">
-              {{state.button_text}}
+            <n-button
+              style="border-radius: 20px; font-size: 15px"
+              type="success"
+              @click="changeStatus"
+              :disabled="!state.send_valid"
+            >
+              {{ state.button_text }}
             </n-button>
           </n-space>
-        </n-space> 
+        </n-space>
       </n-grid-item>
       <n-grid-item span="0:24 640:10 1024:10">
-          <n-space :vertical="true" >
-            <n-space>
-              <n-icon :size="25">
-                <PersonOutline />
-              </n-icon>
-              <n-text style="font-size:18px">
-                用户名
-              </n-text>
-            </n-space>
-            <n-input :disabled="!state.edit_status" v-model:value="state.user.user_name" placeholder="请输入用户名" :on-blur="checkUsername"/>
-            <n-text v-if="state.user.user_valid == false" style="color:red">
-              用户名不符合格式
-            </n-text>
+        <n-space :vertical="true">
+          <n-space>
+            <n-icon :size="25">
+              <PersonOutline />
+            </n-icon>
+            <n-text style="font-size: 18px"> 用户名 </n-text>
           </n-space>
+          <n-input
+            :disabled="!state.edit_status"
+            v-model:value="state.user.user_name"
+            placeholder="请输入用户名"
+            :on-blur="checkUsername"
+          />
+          <n-text v-if="state.user.user_valid == false" style="color: red">
+            用户名不符合格式
+          </n-text>
+        </n-space>
         <!-- </n-card> -->
       </n-grid-item>
       <n-grid-item span="0:24 640:14 1024:14">
         <!-- <n-card class="card_border" style="width: 100%;"> -->
-          <n-space :vertical="true" >
-            <n-space>
-              <n-icon :size="25">
-                <MailOutline />
-              </n-icon>
-              <n-text style="font-size:18px">
-                邮箱
-              </n-text>
-            </n-space>
-            <n-input :disabled="!state.edit_status" v-model:value="state.user.mail" placeholder="请输入邮箱" :on-blur="checkMail"/>
-            <n-text v-if="state.user.mail_valid == false" style="color:red">
-              邮箱不符合格式
-            </n-text>
+        <n-space :vertical="true">
+          <n-space>
+            <n-icon :size="25">
+              <MailOutline />
+            </n-icon>
+            <n-text style="font-size: 18px"> 邮箱 </n-text>
           </n-space>
+          <n-input
+            :disabled="!state.edit_status"
+            v-model:value="state.user.mail"
+            placeholder="请输入邮箱"
+            :on-blur="checkMail"
+          />
+          <n-text v-if="state.user.mail_valid == false" style="color: red">
+            邮箱不符合格式
+          </n-text>
+        </n-space>
       </n-grid-item>
       <n-grid-item span="0:24 640:24 1024:24">
-          <n-space :vertical="true" >
-            <n-space>
-              <n-icon :size="25">
-                <PencilOutline />
-              </n-icon>
-              <n-text style="font-size:18px">
-                个性签名
-              </n-text>
-            </n-space>
-            <n-input type="textarea" :autosize="{
-              minRows: 1,
-              maxRows: 3
-            }" 
-            :disabled="!state.edit_status" v-model:value="state.user.signature" placeholder="请输入个人签名" :on-blur="checkSignature"/>
-            <n-text v-if="state.user.sign_valid == false" style="color:red">
-              个人签名不符合格式（个性签名长度不超过50）
-            </n-text>
+        <n-space :vertical="true">
+          <n-space>
+            <n-icon :size="25">
+              <PencilOutline />
+            </n-icon>
+            <n-text style="font-size: 18px"> 个性签名 </n-text>
           </n-space>
+          <n-input
+            type="textarea"
+            :autosize="{
+              minRows: 1,
+              maxRows: 3,
+            }"
+            :disabled="!state.edit_status"
+            v-model:value="state.user.signature"
+            placeholder="请输入个人签名"
+            :on-blur="checkSignature"
+          />
+          <n-text v-if="state.user.sign_valid == false" style="color: red">
+            个人签名不符合格式（个性签名长度不超过50）
+          </n-text>
+        </n-space>
       </n-grid-item>
       <n-grid-item span="0:24 640:24 1024:24">
         <n-space>
           <n-icon :size="25">
             <EyeOutline />
           </n-icon>
-          <n-text style="font-size:18px">
-            最近浏览
-          </n-text>
+          <n-text style="font-size: 18px"> 最近浏览 </n-text>
         </n-space>
-        <WordChart :tags="props.user.tags == undefined ? {} : props.user.tags"></WordChart>
+        <WordChart
+          :tags="props.user.tags == undefined ? {} : props.user.tags"
+        ></WordChart>
       </n-grid-item>
     </n-grid>
   </div>
@@ -109,6 +129,8 @@ export interface UserInfo {
 
 // Message box
 const message = useMessage();
+
+// const userRef:UserInfo = inject('userRef')
 
 const props = defineProps<{
   user:UserInfo,
